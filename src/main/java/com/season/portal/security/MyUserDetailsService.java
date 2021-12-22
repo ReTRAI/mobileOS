1963fa9c2c326900cc738c6ee86e6a5ed14b6a86
Retrai <geral@retrai.co>
2021-12-22 19:49:03 +0000

    implementação spring security authentication

---
+++ src/main/java/com/season/portal/security/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.season.portal.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails result = null;
+
+        /*
+        User u = WS.getUserByName(username);
+        if(u == null)
+            throw new UsernameNotFoundException("User not found");
+        result = new MyUserDetails(u);
+
+        /**/
+
+        return result;
+    }
+}
