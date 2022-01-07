905d6d7dfe9d8dd58173c43f635ce22abf7349ca
Retrai <geral@retrai.co>
2022-01-07 12:46:22 +0000

    implement cache control headers in all requests

---
+++ src/main/java/com/season/portal/security/HeadersFilter.java
@@ -0,0 +1,24 @@
+package com.season.portal.security;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class HeadersFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        httpServletResponse.addHeader("Cache-Control", "private, no-cache, no-store, must-revalidate");
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
