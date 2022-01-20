ee58b76155a63ed6c9a5f4859d4566472ecba63a
Retrai <geral@retrai.co>
2022-01-20 10:42:39 +0000

    teste git

--- src/main/java/com/season/portal/PortalApplication.java
+++ src/main/java/com/season/portal/PortalApplication.java
@@ -22,7 +22,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
+//teste
 import static com.season.portal.utils.Utils.certificateExpireIn;
 
 @SpringBootApplication
